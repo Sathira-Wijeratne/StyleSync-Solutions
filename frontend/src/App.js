248a3013970eaad7708c8a0c0787af8b6f4ee404
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2023-08-18 14:34:16 +0530

    Added header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
-import './App.css';
+import "./App.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import TestFile from './components/TestFile';
+import TestFile from "./components/TestFile";
+import Header from "./components/Header";
 
 function App() {
   return (
     <Router>
-      <Route path = "/" exact component = {TestFile}/>
+      <Header />
+      <Route path="/" exact component={TestFile} />
     </Router>
   );
 }
