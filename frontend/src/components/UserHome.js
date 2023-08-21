7a481ac59814c4b0de175085942f9e84d19bdda1
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2023-08-21 14:14:49 +0530

    Created customer login

--- frontend/src/components/UserHome.js
+++ frontend/src/components/UserHome.js
@@ -1,9 +1,22 @@
 import React from "react";
+import Button from "react-bootstrap/Button";
 
 export default function UserHome() {
   return (
-    <div>
-      <h1>Welcome User</h1>
+    <div className="container">
+      <div style={{ float: "right" }}>
+        <a
+          href="/"
+          onClick={() => {
+            sessionStorage.removeItem("sSyncSolRemotsuc");
+            sessionStorage.removeItem("customerEmail");
+          }}
+        >
+          {/*Buttons*/}
+          <Button variant="danger">Signout</Button>
+        </a>
+      </div>
+      <h1>Welcome Customer</h1>
     </div>
   );
 }
