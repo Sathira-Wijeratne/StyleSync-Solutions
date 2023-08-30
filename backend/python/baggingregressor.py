b1089679abddd9adb2b42bdfcbd37d6946d9d3ff
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2023-08-30 08:57:32 +0530

    Gathered the relevant data columns

--- backend/python/baggingregressor.py
+++ backend/python/baggingregressor.py
@@ -2,3 +2,7 @@ import pandas as pd
 
 # ---- IMPORTING DATASET ----
 dataset = pd.read_csv('../data/summer-products-with-rating-and-performance_2020-08.csv')
+
+# ---- DATA PREPROCESSING ----
+preprocessedData = dataset[['price', 'units_sold', 'product_variation_size_id', 'rating']]
+
