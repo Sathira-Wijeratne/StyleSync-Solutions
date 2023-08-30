aab590042872f3528c16037a047cd875159d9c12
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2023-08-30 11:15:05 +0530

    Split training and testing data

--- backend/python/baggingclassifier.py
+++ backend/python/baggingclassifier.py
@@ -93,3 +93,6 @@ x = preprocessedData.drop(['units_sold'], axis=1)
 y = preprocessedData['units_sold']
 
 featureList = list(x.columns)
+
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=41)
