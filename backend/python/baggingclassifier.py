ded653184e841f4de9c5ec753084bf1ab2e69240
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2023-08-30 10:18:29 +0530

    Removed records with NaN

--- backend/python/baggingclassifier.py
+++ backend/python/baggingclassifier.py
@@ -1,8 +1,14 @@
 import pandas as pd
 
+# ---------------------------
 # ---- IMPORTING DATASET ----
+# ---------------------------
 dataset = pd.read_csv('../data/summer-products-with-rating-and-performance_2020-08.csv')
 
+# ----------------------------
 # ---- DATA PREPROCESSING ----
-preprocessedData = dataset[['price', 'units_sold', 'product_variation_size_id', 'rating']]
+# ----------------------------
+preprocessedData = dataset[['price', 'units_sold', 'product_variation_size_id', 'rating_count']]
 
+# If NaN, drop records
+preprocessedData.dropna(inplace=True)
