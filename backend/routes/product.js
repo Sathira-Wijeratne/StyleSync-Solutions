28a8deaea83100a517e97dcd00236f35db43e7f7
Sathira Wijeratne <swije27@gmail.com>
2023-09-05 16:41:23 +0530

    implemented route to get single product details

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -2,10 +2,10 @@ const router = require("express").Router();
 const fs = require("fs");
 const { parse } = require("csv-parse");
 
-// let products = [];
+let products = [];
 
 router.route("/").get(async (req, res) => {
-  let products = [];
+  // let products = [];
   if (products.length === 0) {
     fs.createReadStream(
       "./data/summer-products-with-rating-and-performance_2020-08.csv"
@@ -28,47 +28,38 @@ router.route("/").get(async (req, res) => {
   } else {
     res.json(products);
   }
-
 });
 
-// router.route("/getDetails/:id").get(async (req, res) => {
-//   let products = [];
+router.route("/getDetails/:id").get(async (req, res) => {
+  // let products = [];
+  let productId = req.params.id;
 
-//   fs.createReadStream(
-//     "./data/summer-products-with-rating-and-performance_2020-08.csv"
-//   )
-//     .pipe(parse({ delimiter: ",", from_line: 2, to_line: 5 }))
-//     .on("data", function (row) {
-//       products.push(row);
-//     })
-//     .on("end", function () {
-//       console.log(products);
-//       console.log("Finished");
-//       // res.json(products);
-//     })
-//     .on("error", function (error) {
-//       console.log(error.message);
-//       // res.status(500).send({
-//       //   status: "Error with getting the products",
-//       //   error: error.message,
-//       // });
-//     });
-
-//   let productId = req.params.id;
-//   console.log(productId);
-//   console.log("hello");
-
-//   const productDetails = products.find((product) => product[40] == productId);
-
-//   // if (products.length === 0) {
-//   //   res.status(500).send({ status: "Error with get product", error: "Main product list empty" });
-//   // }
-//   // else if (productDetails.length !== 0) {
-//   //   res.status(200).send({ status: "Product fetched", productDetails });
-//   //   // res.json(productDetails);
-//   // } else {
-//   //   res.status(500).send({ status: "Error with get product", error: "Product not found" });
-//   // }
-// })
+  if (products.length === 0) {
+    fs.createReadStream(
+      "./data/summer-products-with-rating-and-performance_2020-08.csv"
+    )
+      .pipe(parse({ delimiter: ",", from_line: 2, to_line: 5 }))
+      .on("data", function (row) {
+        products.push(row);
+      })
+      .on("end", function () {
+        const productDetails = products.find((product) => product[40] == productId);
+        console.log(productDetails);
+        res.json(productDetails);
+        console.log("Product fetching finished");
+      })
+      .on("error", function (error) {
+        console.log(error.message);
+        res.status(500).send({
+          status: "Error with fetching the product",
+          error: error.message,
+        });
+      });
+  } else {
+    const productDetails = products.find((product) => product[40] == productId);
+    res.json(productDetails);
+    console.log("Product fetching finished");
+  }
+})
 
 module.exports = router;
