305fddf33081a055fabc3d41449ed83a4bd2049b
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2023-09-06 21:45:31 +0530

    Added style to table

--- frontend/src/components/SalesForecast.js
+++ frontend/src/components/SalesForecast.js
@@ -2,18 +2,64 @@ import React from "react";
 
 export default function SalesForecast() {
   return (
-    <div>
+    <div className="container">
       <center>
         <h1>Sales Forecast</h1>
-        <table border={2}>
-          <tr>
-            <th>Product Size</th>
-            <th>Product Price</th>
-          </tr>
-          <tr>
-            <td>XXXS</td>
-            <td></td>
-          </tr>
+        <table className="table" style={{ marginLeft: "20%" }}>
+          <thead>
+            <tr>
+              <th>Product Size</th>
+              <th className="th-sm">Product Price</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>XXXS</td>
+              <td>
+                <input type="number" />
+              </td>
+            </tr>
+            <tr>
+              <td>XXS</td>
+              <td></td>
+            </tr>
+            <tr>
+              <td>XS</td>
+              <td></td>
+            </tr>
+            <tr>
+              <td>S</td>
+              <td></td>
+            </tr>
+            <tr>
+              <td>M</td>
+              <td></td>
+            </tr>
+            <tr>
+              <td>L</td>
+              <td></td>
+            </tr>
+            <tr>
+              <td>XL</td>
+              <td></td>
+            </tr>
+            <tr>
+              <td>XXL</td>
+              <td></td>
+            </tr>
+            <tr>
+              <td>XXXL</td>
+              <td></td>
+            </tr>
+            <tr>
+              <td>XXXXL</td>
+              <td></td>
+            </tr>
+            <tr>
+              <td>XXXXXL</td>
+              <td></td>
+            </tr>
+          </tbody>
         </table>
       </center>
     </div>
