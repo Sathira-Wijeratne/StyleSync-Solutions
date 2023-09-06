a294e44fa29f76cc43b38050310f5f44b610e1f4
Sathira Wijeratne <swije27@gmail.com>
2023-09-06 17:31:47 +0530

    formatted code

--- frontend/src/components/UserHome.js
+++ frontend/src/components/UserHome.js
@@ -22,7 +22,10 @@ export default function UserHome() {
   }, []);
 
   return (
-    <div className="container" style={{ background: "#eee", padding: '3em', borderRadius: '20px' }}>
+    <div
+      className="container"
+      style={{ background: "#eee", padding: "3em", borderRadius: "20px" }}
+    >
       <div id="product-page-buttons" style={{ float: "right" }}>
         <a
           href="/"
@@ -38,7 +41,10 @@ export default function UserHome() {
       <div id="product-page-heading" style={{ textAlign: "center" }}>
         <h1>All Summer Products ☀️</h1>
       </div>
-      <div id="product-page-body" style={{ marginTop: "40px", marginBottom: "40px" }}>
+      <div
+        id="product-page-body"
+        style={{ marginTop: "40px", marginBottom: "40px" }}
+      >
         <div
           style={{
             display: "grid",
@@ -55,12 +61,15 @@ export default function UserHome() {
                 alignItems: "center",
                 padding: "1rem",
                 border: "5px solid #32a852",
-                borderRadius: "20px"
+                borderRadius: "20px",
               }}
               key={product[40]}
             >
               <div className="grow">
-                <img src={product[39]} style={{ maxWidth: "100%", borderRadius: "10px" }} />
+                <img
+                  src={product[39]}
+                  style={{ maxWidth: "100%", borderRadius: "10px" }}
+                />
               </div>
               <div
                 style={{
@@ -76,9 +85,16 @@ export default function UserHome() {
                 <span style={{ fontWeight: "bold", marginBottom: "0.5rem" }}>
                   {parseFloat(product[2]).toFixed(2)} €
                 </span>
-                <button className="btn btn-secondary" onClick={() => {
-                  window.location.replace(`http://localhost:3000/viewproduct/${product[40]}`);
-                }}>View</button>
+                <button
+                  className="btn btn-secondary"
+                  onClick={() => {
+                    window.location.replace(
+                      `http://localhost:3000/viewproduct/${product[40]}`
+                    );
+                  }}
+                >
+                  View
+                </button>
               </div>
             </div>
           ))}
