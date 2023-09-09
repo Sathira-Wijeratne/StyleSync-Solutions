f9d974676f2787d1e5d8ac9c38a2f046580c7202
SanuthiVihansa <subasofts@gmail.com>
2023-09-09 06:58:07 +0530

    commit productratingreport page

---
+++ frontend/src/components/ProductRatingReport.js
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from "react";
+
+function ProductRatingReport() {
+    const [rating,setRating]= useState([]);
+
+    useEffect(()=>{
+
+},[]);
+
+return(
+    <div>
+
+    </div>
+);
+}
+
+export default ProductRatingReport;
