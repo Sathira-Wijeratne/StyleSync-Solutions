1357c2ae870ae963f115b2ae822b851de756f9b6
SanuthiVihansa <subasofts@gmail.com>
2023-09-09 13:46:07 +0530

    commit productratingreport changes

--- frontend/src/components/ProductRatingReport.js
+++ frontend/src/components/ProductRatingReport.js
@@ -4,11 +4,28 @@ function ProductRatingReport() {
     const [rating,setRating]= useState([]);
 
     useEffect(()=>{
+        fetch(`http://localhost:8070/rating/`).then((response)=>response.json()).then((response)=>response.json()).then((data)=>{
+            setRating(data);
+        }).catch((error)=>{
+            console.error("Error fetching rating",error);
+        });
 
 },[]);
 
 return(
     <div>
+        <h1>
+            Rating Report 
+        </h1>
+        <ul>
+        {
+            rating.map((rating)=>(
+                <li key={rating._id}>
+                    Item Name: {rating.title_orig}, Rating: {rating.noOfRate}
+                </li>
+            ))
+        }
+        </ul>
 
     </div>
 );
