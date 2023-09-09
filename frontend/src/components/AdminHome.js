40d4c08211be1e031b72ba329222c674aafcf8ae
HilarinaChristy <it21127946@my.sliit.lk>
2023-09-09 22:34:34 +0530

    Added routes for "Reports and Analytics"

--- frontend/src/components/AdminHome.js
+++ frontend/src/components/AdminHome.js
@@ -1,91 +1,3 @@
-// import React from "react";
-// import Button from "react-bootstrap/Button";
-// import card1Image from "../images/card1.jpg";
-// import card2Image from "../images/card2.jpg";
-// import card3Image from "../images/card3.jpg";
-// import card4Image from "../images/card4.jpg";
-// import backgroundImage from "../images/background-admin.jpeg";
-
-// export default function AdminHome() {
-//   const containerStyle = {
-//     backgroundImage: `url(${backgroundImage})`, // Set the background image
-//     backgroundSize: "cover", // Cover the entire container
-//     backgroundRepeat: "no-repeat", // Don't repeat the background image
-//     backgroundAttachment: "fixed", // Keep the background image fixed
-//     backgroundPosition: "center", // Center the background image
-//     minHeight: "100vh", // Make the container at least the height of the viewport
-//     display: "flex",
-//     flexDirection: "column",
-//     alignItems: "center",
-//     justifyContent: "center",
-//     color: "white",
-//   };
-
-//   return (
-//     <div className="container" style={containerStyle}>
-//       style=
-//       {{
-//         backgroundImage: `url(${backgroundImage})`, // Set the background image
-//         backgroundSize: "cover", // Cover the entire container
-//         backgroundRepeat: "no-repeat", // Don't repeat the background image
-//         backgroundAttachment: "fixed", // Keep the background image fixed
-//         backgroundPosition: "center", // Center the background image
-//         minHeight: "100vh", // Make the container at least the height of the viewport
-//         display: "flex",
-//         flexDirection: "column",
-//         alignItems: "center",
-//         justifyContent: "center",
-//         color: "white",
-//       }}
-//       <div style={{ float: "right" }}>
-//         <a
-//           href="/"
-//           onClick={() => {
-//             sessionStorage.removeItem("sSyncSolNimda");
-//             sessionStorage.removeItem("adminEmail");
-//           }}
-//         >
-//           {/*Buttons*/}
-//           <Button variant="danger">Signout</Button>
-//           {/* Display four cards */}
-
-//           <h1 className="admin-heading">Admin Dashboard</h1>
-
-//           <div className="card-matrix">
-//             <a href="/adminhome/discount/add">
-//               {/* Different-colored cards */}
-//               <div className="card card-blue-hil">
-//                 <img src={card1Image} alt="Card 1" className="card-image" />
-//                 <div className="card-content">Add Discounts</div>
-//               </div>
-//             </a>
-
-//             <a href="/adminhome/discount/">
-//               <div className="card card-green-hil">
-//                 <img src={card2Image} alt="Card 2" className="card-image" />
-//                 <div className="card-content">View Discounts</div>
-//               </div>
-//             </a>
-//             <a href="/adminhome/discount/update/">
-//               <div className="card card-orange-hil">
-//                 <img src={card3Image} alt="Card 3" className="card-image" />
-//                 <div className="card-content">Update Discounts</div>
-//               </div>
-//             </a>
-
-//             <a href="/adminhome/discount/reports">
-//               <div className="card card-purple-hil">
-//                 <img src={card4Image} alt="Card 4" className="card-image" />
-//                 <div className="card-content">Generate Reports</div>
-//               </div>
-//             </a>
-//           </div>
-//         </a>
-//       </div>
-//       <h1>Welcome Admin</h1>
-//     </div>
-//   );
-// }
 import React from "react"; // Import your background image
 import Card from "react-bootstrap/Card";
 import CardImg from "react-bootstrap/CardImg";
@@ -265,12 +177,14 @@ export default function AdminHome() {
               justifyContent: "center",
             }}
           >
-            <Card.Title>Reports and Analytics</Card.Title>
-            <FontAwesomeIcon
-              icon={faChartLine}
-              style={{ fontSize: "48px", marginTop: "10px" }}
-            />
-            {/* <Card.Text>Card content here.</Card.Text> */}
+            <a href="/adminhome/discount/reports" style={linkStyle}>
+              <Card.Title>Reports and Analytics</Card.Title>
+              <FontAwesomeIcon
+                icon={faChartLine}
+                style={{ fontSize: "48px", marginTop: "10px" }}
+              />
+              {/* <Card.Text>Card content here.</Card.Text> */}
+            </a>
           </Card.Body>
         </Card>
       </div>
