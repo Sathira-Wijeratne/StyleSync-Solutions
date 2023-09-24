a374da912e5a7a0d57f3d0cc94e6617c052e3b3f
SanuthiVihansa <subasofts@gmail.com>
2023-09-24 11:50:36 +0530

    commit header component modifications

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,31 +1,54 @@
 import React from "react";
 import logo from "../images/logo.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCartShopping,
+  faClockRotateLeft,
+  faCircleUser
+  
+} from "@fortawesome/free-solid-svg-icons";
 
 export default function Header() {
   return (
-    // <div>
-    //   <nav className="navbar fixed-top bg-dark" style={{ color: "White" }}>
-    //     <div className="row">
-    //       <div className="col-3">
-    //         &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-    //         <img src={logo} width="40%" alt="Logo"></img>
-    //       </div>
-    //       <div className="col" style={{ fontSize: "70px" }}>
-    //         <b>StyleSync Summer Store</b>
-    //       </div>
-    //     </div>
-    //   </nav>
-    //   <br></br>
-    //   <br></br>
-    //   <br></br>
-    //   <br></br>
-    //   <br></br>
-    //   <br></br>
-    //   <br></br>
-    // </div>
-    <div class="container-fluid" style={{backgroundColor:"Black"}}>
-    <img src={logo} width={80} height={80}  alt="Logo"></img>
+    <div className="container-fluid" style={{ backgroundColor: "black" }}>
+      <div className="row">
+        <div className="col-md-12 text-left">
+          <img src={logo} width={80} height={80} alt="Logo" />
+        </div>
+        <div className="col-md-12 text-end" style={{ justifyContent: "center" }}>
 
+          <a type="button" href="/cart">
+            <FontAwesomeIcon
+              icon={faCartShopping}
+              shake
+              size="m"
+              style={{ marginLeft: "25px" ,color:"White"}}
+            />
+          </a>
+          <a type="button" href="/purchaseHistory">
+            <FontAwesomeIcon
+              icon={faClockRotateLeft}
+              size="m"
+              style={{ marginLeft: "25px",color:"White"}}
+            />
+          </a>
+          <a
+            href="/"
+            onClick={() => {
+              sessionStorage.removeItem("sSyncSolRemotsuc");
+              sessionStorage.removeItem("customerEmail");
+            }}
+          >
+             <a type="button">
+            <FontAwesomeIcon
+              icon={faCircleUser}
+              size="m"
+              style={{ marginLeft: "25px", color:"White"}}
+            />
+          </a>
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
