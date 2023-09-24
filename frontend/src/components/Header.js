e1e5eeff0a61178f494071eb9b39236848950953
SanuthiVihansa <subasofts@gmail.com>
2023-09-24 12:17:38 +0530

    commit header.js modifications

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,54 +1,97 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../images/logo.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faCartShopping,
   faClockRotateLeft,
-  faCircleUser
-  
+  faUserCircle,
 } from "@fortawesome/free-solid-svg-icons";
 
 export default function Header() {
+  const [showDropdown, setShowDropdown] = useState(false); // State to control dropdown visibility
+
+  // Function to toggle dropdown visibility
+  const toggleDropdown = () => {
+    setShowDropdown(!showDropdown);
+  };
+
   return (
     <div className="container-fluid" style={{ backgroundColor: "black" }}>
-      <div className="row">
-        <div className="col-md-12 text-left">
-          <img src={logo} width={80} height={80} alt="Logo" />
-        </div>
-        <div className="col-md-12 text-end" style={{ justifyContent: "center" }}>
-
-          <a type="button" href="/cart">
-            <FontAwesomeIcon
-              icon={faCartShopping}
-              shake
-              size="m"
-              style={{ marginLeft: "25px" ,color:"White"}}
-            />
-          </a>
-          <a type="button" href="/purchaseHistory">
-            <FontAwesomeIcon
-              icon={faClockRotateLeft}
-              size="m"
-              style={{ marginLeft: "25px",color:"White"}}
-            />
-          </a>
-          <a
-            href="/"
-            onClick={() => {
-              sessionStorage.removeItem("sSyncSolRemotsuc");
-              sessionStorage.removeItem("customerEmail");
-            }}
-          >
-             <a type="button">
-            <FontAwesomeIcon
-              icon={faCircleUser}
-              size="m"
-              style={{ marginLeft: "25px", color:"White"}}
-            />
-          </a>
-          </a>
-        </div>
-      </div>
+      <table className="borderless">
+        <tbody>
+          <tr>
+            <td>
+              <div className="col-md-12 text-left">
+                <img src={logo} width={80} height={80} alt="Logo" />
+              </div>
+            </td>
+            <td width={1050}></td>
+            <td>
+              <div
+                className="col-md-12 text-end"
+                style={{ justifyContent: "right", display: "flex" }}
+              >
+                <a type="button" href="/cart">
+                  <FontAwesomeIcon
+                    icon={faCartShopping}
+                    shake
+                    size="1x" // Change to "1x" for normal size
+                    style={{ marginLeft: "25px", color: "white" }}
+                  />
+                </a>
+                <a type="button" href="/purchaseHistory">
+                  <FontAwesomeIcon
+                    icon={faClockRotateLeft}
+                    size="1x" // Change to "1x" for normal size
+                    style={{ marginLeft: "25px", color: "white" }}
+                  />
+                </a>
+                <div
+                  onClick={toggleDropdown}
+                  style={{ cursor: "pointer", marginLeft: "25px" }}
+                >
+                  <FontAwesomeIcon
+                    icon={faUserCircle}
+                    size="1x" // Change to "1x" for normal size
+                    style={{ color: "white" }}
+                  />
+                </div>
+                {/* Dropdown menu */}
+                {showDropdown && (
+                  <div className="dropdown" style={{ marginLeft: "25px" }}>
+                    <button
+                      className="btn btn-danger dropdown-toggle"
+                      type="button"
+                      id="dropdownMenuButton"
+                      data-toggle="dropdown"
+                      aria-haspopup="true"
+                      aria-expanded="false"
+                      style={{ backgroundColor: "black", border: "none" }}
+                    >
+                      Sign Out
+                    </button>
+                    <div
+                      className="dropdown-menu"
+                      aria-labelledby="dropdownMenuButton"
+                    >
+                      <a
+                        className="dropdown-item"
+                        href="/"
+                        onClick={() => {
+                          sessionStorage.removeItem("sSyncSolRemotsuc");
+                          sessionStorage.removeItem("customerEmail");
+                        }}
+                      >
+                        Sign Out
+                      </a>
+                    </div>
+                  </div>
+                )}
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
