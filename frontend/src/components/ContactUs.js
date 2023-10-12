2f9d63f656112abe3b043ea00742857c07c556ef
SanuthiVihansa <subasofts@gmail.com>
2023-10-12 23:44:02 +0530

    commit form validations done

--- frontend/src/components/ContactUs.js
+++ frontend/src/components/ContactUs.js
@@ -1,15 +1,16 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 export default function ContactUs() {
+  const [name, setName] = useState("");
+  const [nameError, setNameError] = useState("");
+  const [email, setEmail] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [phoneNumberError, setPhoneNumberError] = useState("");
+
   const mainDivStyle = {
     margin: "100px", // Add margin
   };
-  const [name, setName] = useState('');
-  const [nameError, setNameError] = useState('');
-  const [email, setEmail] = useState('');
-  const [emailError, setEmailError] = useState('');
-  const [phoneNumber, setPhoneNumber] = useState('');
-  const [phoneNumberError, setPhoneNumberError] = useState('');
 
   const handleNameChange = (e) => {
     const value = e.target.value;
@@ -18,11 +19,11 @@ export default function ContactUs() {
     const pattern = /^[A-Za-z\s]+$/;
 
     if (!value) {
-      setNameError('Name cannot be empty.');
+      setNameError("Name cannot be empty.");
     } else if (!pattern.test(value)) {
-      setNameError('Name should only contain letters and spaces.');
+      setNameError("Name should only contain letters and spaces.");
     } else {
-      setNameError('');
+      setNameError("");
     }
   };
 
@@ -33,11 +34,11 @@ export default function ContactUs() {
     const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
 
     if (!value) {
-      setEmailError('Email address cannot be empty.');
+      setEmailError("Email address cannot be empty.");
     } else if (!emailPattern.test(value)) {
-      setEmailError('Please enter a valid email address.');
+      setEmailError("Please enter a valid email address.");
     } else {
-      setEmailError('');
+      setEmailError("");
     }
   };
 
@@ -48,17 +49,56 @@ export default function ContactUs() {
     const phoneNumberPattern = /^[\d\- ]*$/;
 
     if (!value) {
-      setPhoneNumberError('Contact Number cannot be empty.');
+      setPhoneNumberError("Contact Number cannot be empty.");
     } else if (!phoneNumberPattern.test(value)) {
-      setPhoneNumberError('Please enter a valid phone number.');
+      setPhoneNumberError("Please enter a valid phone number.");
     } else {
-      setPhoneNumberError('');
+      setPhoneNumberError("");
     }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault(); // Prevent the default form submission
+
+    // Check if any of the required fields are empty
+    if (!name || !email || !phoneNumber) {
+      // Display an error message or handle it as needed
+      alert("All fields are required");
+      return;
+    }
+
+    // Prepare the data to be sent to the server
+    const formData = {
+      name,
+      email,
+      phone: phoneNumber, // Assuming the field name is 'phone' on the server
+      Inquery: inqure, // You can set this as needed
+    };
+
+    // Send a POST request to your server to add the data
+    fetch("http://localhost:8070/ContactUs/add", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        // Handle the response from the server, e.g., show a success message
+        console.log(data);
+        alert("Inquiry Added.");
+      })
+      .catch((error) => {
+        // Handle any errors, e.g., show an error message
+        console.error(error);
+        alert("An error occurred. Please try again.");
+      });
   };
 
   return (
     <div style={mainDivStyle}>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
           <label>Name</label>
           <input
@@ -89,12 +129,19 @@ export default function ContactUs() {
             value={phoneNumber}
             onChange={handlePhoneNumberChange}
           />
-          {phoneNumberError && <div className="text-danger">{phoneNumberError}</div>}
+          {phoneNumberError && (
+            <div className="text-danger">{phoneNumberError}</div>
+          )}
         </div>
 
         <div className="form-group">
           <label for="exampleInputPassword1">Inquiry</label>
-          <input type="text" className="form-control" id="exampleInputPassword1" />
+          <input
+            type="text"
+            className="form-control"
+            id="exampleInputPassword1"
+            value={inqure}
+          />
         </div>
 
         <button type="submit" className="btn btn-primary">
