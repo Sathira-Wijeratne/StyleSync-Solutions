0ddbd493d5cb24d82cfd1f661c42fb8e89ef3eb3
SanuthiVihansa <subasofts@gmail.com>
2023-10-12 23:10:30 +0530

    commit contact us adjustments

--- frontend/src/components/ContactUs.js
+++ frontend/src/components/ContactUs.js
@@ -1,73 +1,37 @@
 import React from 'react';
 
 export default function ContactUs() {
+    const mainDivStyle = {
+        margin: '100px', // You can adjust the margin size as needed
+      };
+
   return (
-    <div>
-      <form className="needs-validation" noValidate>
-        <div className="form-row">
-          <div className="col-md-4 mb-3">
-            <label htmlFor="validationCustom01">First name</label>
-            <input type="text" className="form-control" id="validationCustom01" placeholder="First name" defaultValue="Mark" required />
-            <div className="valid-feedback">
-              Looks good!
-            </div>
-          </div>
-          <div className="col-md-4 mb-3">
-            <label htmlFor="validationCustom02">Last name</label>
-            <input type="text" className="form-control" id="validationCustom02" placeholder="Last name" defaultValue="Otto" required />
-            <div className="valid-feedback">
-              Looks good!
-            </div>
-          </div>
-          <div className="col-md-4 mb-3">
-            <label htmlFor="validationCustomUsername">Username</label>
-            <div className="input-group">
-              <div className="input-group-prepend">
-                <span className="input-group-text" id="inputGroupPrepend">@</span>
-              </div>
-              <input type="text" className="form-control" id="validationCustomUsername" placeholder="Username" aria-describedby="inputGroupPrepend" required />
-              <div className="invalid-feedback">
-                Please choose a username.
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="form-row">
-          <div className="col-md-6 mb-3">
-            <label htmlFor="validationCustom03">City</label>
-            <input type="text" className="form-control" id="validationCustom03" placeholder="City" required />
-            <div className="invalid-feedback">
-              Please provide a valid city.
-            </div>
-          </div>
-          <div className="col-md-3 mb-3">
-            <label htmlFor="validationCustom04">State</label>
-            <input type="text" className="form-control" id="validationCustom04" placeholder="State" required />
-            <div className="invalid-feedback">
-              Please provide a valid state.
-            </div>
-          </div>
-          <div className="col-md-3 mb-3">
-            <label htmlFor="validationCustom05">Zip</label>
-            <input type="text" className="form-control" id="validationCustom05" placeholder="Zip" required />
-            <div className="invalid-feedback">
-              Please provide a valid zip.
-            </div>
-          </div>
-        </div>
-        <div className="form-group">
-          <div className="form-check">
-            <input className="form-check-input" type="checkbox" value="" id="invalidCheck" required />
-            <label className="form-check-label" htmlFor="invalidCheck">
-              Agree to terms and conditions
-            </label>
-            <div className="invalid-feedback">
-              You must agree before submitting.
-            </div>
-          </div>
-        </div>
-        <button className="btn btn-primary" type="submit">Submit form</button>
-      </form>
+    <div style={mainDivStyle}>
+      <form>
+      <div class="form-group">
+      <label for="exampleInputPassword1">Name</label>
+      <input type="text" class="form-control" id="exampleInputPassword1"/>
+    </div>
+
+  <div class="form-group">
+    <label for="exampleInputEmail1">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
+    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+  </div>
+ 
+  
+      <div class="form-group">
+    <label for="exampleInputPassword1">Contact Number</label>
+    <input type="text" class="form-control" id="exampleInputPassword1"/>
+  </div>
+
+  <div class="form-group">
+    <label for="exampleInputPassword1">Inqury</label>
+    <input type="text" class="form-control" id="exampleInputPassword1"/>
+  </div>
+
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
     </div>
   );
 }
